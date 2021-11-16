4c36a0374ae85aadea82727ab491f005bb587515
Rohini0702 <rohini.nagtilak@zingworks.in>
2021-11-16 15:29:08 +0530

    i am learning basic git commands

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am learning git and github for CI/CD</h1>
+      <p>First you have to learn, how to learn</p>
+      <ul>
+        <li>Rohini</li>
+        <li>Komal</li>
+      </ul>
+      <p>Lets start to learn with coding and enjoy..</p>
+    </div> 
+  );
+}
+
+export default App;
